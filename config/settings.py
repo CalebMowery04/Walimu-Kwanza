c00ba2698aabbdcfbafd06970fe3decfecddd9fc
Caleb Mowery <calebmowery7@gmail.com>
2026-09-16 04:18:20 -0400

    Add CSRF trusted origins setting

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-8x2ad7qk$rc(3i
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
+CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', 'http://localhost').split()
 
 
 # Application definition
